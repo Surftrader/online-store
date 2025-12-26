7d7d17f3b8a048e368912853a62baa4f4a537c04
Sergiy Poddubka <poseal@gmail.com>
2025-12-26 07:52:21 +0200

    add apps

--- shop/settings.py
+++ shop/settings.py
@@ -40,6 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'products',
+    'users',
+    'orders',
+    'reviews',
 ]
 
 MIDDLEWARE = [
